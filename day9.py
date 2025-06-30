015f8a8af59eff132697113aad53033de659484f
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-06-30 13:25:56 +0530

    30th june

---
+++ day9.py
@@ -0,0 +1,37 @@
+# a = "12456"
+
+# ans = a.isdecimal()
+
+# print(ans)
+
+
+# a = "45677²"
+# ans = a.isdigit()
+# print(ans)
+
+# a = "15.65"
+# ans = a.isnumeric()
+# print(ans)
+
+# a = "         "
+# ans = a.isspace()
+# print(ans)
+
+
+# a = "My Name Is Uday Kumar Valapudasu"
+
+# ans = a.istitle()
+# print(ans)
+
+
+# a = "my name is uday kumar valapudasu"
+
+# ans = a.title()
+# print(ans.istitle())
+# print(ans)
+
+
+a = "My name is Rizwana rizwana rizwana"
+
+ans = a.replace("rizwana","kedar")
+print(ans)
