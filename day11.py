895ee6d34c5a1725c03f32a1e744adeab34dc147
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-02 10:38:06 +0530

    day 11 pushed

---
+++ day11.py
@@ -0,0 +1,27 @@
+# print("kedar")
+# print("kedar")
+# print("kedar")
+# print("kedar")
+# print("kedar")
+# print("kedar")
+# print("kedar")
+# print("kedar")
+# print("kedar")
+# print("kedar")
+
+
+# for i in range(1,5,1):
+#     print(i*2)
+
+# sum =0
+
+# for i in range(1,5,1):
+#     sum = sum+i
+# print(sum)
+
+
+# for i in range(10,23,3):
+#     print(i)
+
+for i in range(2,1001,2):
+    print(i)
