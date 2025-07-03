3a88588866f2f9650213bd78b5e621e795606b7f
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-03 10:36:39 +0530

    pushed 11day

--- day11.py
+++ day11.py
@@ -13,9 +13,9 @@
 # for i in range(1,5,1):
 #     print(i*2)
 
-# sum =0
+# sum =10
 
-# for i in range(1,5,1):
+# for i in range(1,5,1): # i = 1 2 3 4
 #     sum = sum+i
 # print(sum)
 
@@ -23,5 +23,48 @@
 # for i in range(10,23,3):
 #     print(i)
 
-for i in range(2,1001,2):
-    print(i)
+# for i in range(2,1001,2):
+#     print(i)
+
+
+
+# Gratest of three numbers
+
+
+# a = 100
+# b = 2000
+# c = 780
+
+
+# if a>b:
+#     if a>c:
+#         print(a)
+#     else:
+#         print(c)
+# elif b>c:
+#     print(b)
+# else:
+#     print(c)
+
+# if a>b and a>c:
+#     print(a)
+# elif b>a and b>c :
+#     print(b)
+# else:
+#     print(c)
+
+
+
+
+
+# 3 6 9 12 15 18 
+
+# for i in range(1,7,1):
+#     print(i*3)
+
+
+# for i in range(1,11):
+#     for j in range(1,6):
+#         if "ball" in j:
+#             print(i,j)
+
