2ce8a27687eb65ec330ffa85cb82b58c11b7c7db
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-04 10:22:56 +0530

    day 12 pushed

---
+++ day12.py
@@ -0,0 +1,24 @@
+# name = "rizwana"
+# for i in name:
+#     print(i)
+
+
+
+# name="kedar"
+# sum = 0
+# for i in name:
+#     if i in "aeiou":
+#         sum = sum+1
+
+# print(sum)
+
+
+name = "uday kumar"
+# print(len(name))
+
+
+sum = 0
+for i in name:
+    sum = sum +1
+
+print(sum)
