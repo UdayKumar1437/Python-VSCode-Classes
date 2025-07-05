b0a5d217d0e73b8b0281aba7de87f4ce7dfe187e
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-05 11:49:48 +0530

    day 13 pushed

---
+++ day13.py
@@ -0,0 +1,20 @@
+# n = 5
+# # 1+2+3+4+5
+# sum = 0
+# for i in range(1,n+1):
+#     sum = sum+i
+# print(sum)
+
+
+# num1 = 5
+# num2 = 9
+# # 5+6+7+8+9
+# sum = 0
+# for i in range(num1,num2+1):
+#     sum=sum+i
+# print(sum)
+
+num = 10
+for i in range(1,num+1):
+    if num%i == 0:
+        print(i)
