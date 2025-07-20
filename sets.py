c72a039b296e3493817c2e8b3c56d99f50071e28
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-20 15:33:09 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,48 @@
+# a = {1,2,3,1}
+# a.add(4)
+# a.clear()
+# print(a)
+
+
+x = {"apple", "banana", "cherry"}
+y = { "apple","banana","uday","cherry"}
+
+# z = x.issuperset(y) # X is the superset of Y
+# print(z)
+
+z = x.issubset(y) # X is the subset of Y
+print(z)
+
+# z = x.union(y)
+# print(z)
+
+# x.update({"guava","pineapple"})
+# print(x)
+
+# x.remove("banana")
+# print(x)
+
+
+# z = x.isdisjoint(y) # Return True when items of x not in y set otherwise returns False
+# print(z)
+
+# z = y.difference(x)
+# print(z)
+
+# y.difference_update(x)
+# print(y)
+
+# z = x.intersection(y)
+# print(z)
+
+
+# x.intersection_update(y)
+# print(x)
+
+
+# z = x.symmetric_difference(y)
+# print(z)
+
+
+# y.symmetric_difference_update(x)
+# print(y)
