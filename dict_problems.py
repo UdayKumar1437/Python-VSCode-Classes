600a829dbe9c6ee4e32d9ac3bd306f4dd25da9b6
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-23 10:23:59 +0530

    pushed

---
+++ dict_problems.py
@@ -0,0 +1,22 @@
+# arr = [10,12,13,45,55,100]
+# ans = {}
+
+# for i in arr:
+#     a = []
+#     for j in range(1,i+1):
+#         if i%j ==0:
+#             a.append(j)
+#     ans[i] = a
+# print(ans)
+
+
+
+
+arr = [1,1,2,3,6,2]
+ans= {}
+for i in arr:
+    ans[i] = ans.get(i,0)+1
+k = ans.keys()
+for i in k:
+    if ans.get(i) == 1:
+        print(i)
